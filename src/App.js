5e1718c67239f029b597b06012799b6678beda4b
佐々木皓晃 <ssk.hrak123@gmail.com>
2020-12-01 17:54:57 +0900

    create arrow function components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-function App() {
+// function App() {
+//   return (
+//     <>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {console.log("チェンジ！")}}/>
+//     </>
+//   );
+// }
+
+const App = () => {
   return (
-    <>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("チェンジ！")}}/>
-    </>
+    <div>
+      <Cat />
+    </div>
   );
+}
+
+const Cat = () => {
+  return (
+    <div>Meow!</div>
+  )
 }
 
 export default App;
